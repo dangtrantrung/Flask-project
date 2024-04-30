f6de11d54657130df1dc0444bfdc03f3265a17cd
dangtrantrung <trung280584@gmail.com>
2024-04-30 14:49:53 +0700

    dynamic Urls params

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask,request
+
+
+
 
 app=Flask(__name__)
 
@@ -6,6 +9,24 @@ app=Flask(__name__)
 def index():
     return "<h1> Flask AI Web App </h1>"
 
+@app.route('/hello')
+def hello():
+    return "Hello World"
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello {name}"
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1,number2):
+    result=int(number1)+int(number2)
+    result2=f"kết quả là: {number1 +number2}"
+    return result2
+# dynamic Urls params
+@app.route('/handle_url_params')
+def handle_params():
+    greet=request.args['greeting']
+    name=request.args['name']
+    age=request.args['age']
+    return f'{greet} {name} {age} year old'
 
 
 if __name__=='__main__':
