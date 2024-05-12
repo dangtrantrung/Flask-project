e7adcdcb9f6cea3fb7c62fc53f3d14dbe174d850
dangtrantrung <trung280584@gmail.com>
2024-05-12 14:09:52 +0700

    Refactoring & design pattern

--- dbapp.py
+++ dbapp.py
@@ -9,6 +9,9 @@ def create_app():
     app=Flask(__name__,template_folder='templates')
     app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///./testdb.db'
     db.init_app(app)
+    # import routes
+    from routes import register_routes
+    register_routes(app,db)
 
     migrate=Migrate(app,db)
 
